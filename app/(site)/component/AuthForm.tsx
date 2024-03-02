c6b3d77165d3415bf64d0b5a43a92d2c7865dd0d
kei-1313 <candydog666@gmail.com>
2024-03-02 19:38:38 +0900

    shadcnを使いformの作成

--- app/(site)/component/AuthForm.tsx
+++ app/(site)/component/AuthForm.tsx
@@ -1,9 +1,100 @@
 "use client"
 
-const AuthForm = () => { 
+//コンポネント
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as z from 'zod';
+import { useForm } from 'react-hook-form';
+
+const formSchema = z.object({
+  name: z.string().min(2).max(50),
+  email: z.string().email(),
+  password: z.string().min(2).max(50),
+});
+
+const AuthForm = () => {
+
+  const form = useForm<z.infer<typeof formSchema>>(
+    {
+      resolver: zodResolver(formSchema),
+      defaultValues: {
+        name: '',
+        email: '',
+        password: ''
+      }
+    }
+  )
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  }
+
+
 	return (
 		<div>
-
+      <Form {...form}>
+        <form className="w-[600px] mt-10 mx-auto" onSubmit={form.handleSubmit(onSubmit)}>
+          <div className="mb-6">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Username" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="mb-6">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Username" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="mb-6">
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Username" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="text-center mt-8">
+            <Button type="submit" variant="outline" size="lg" className="shadow-xl">Sign up</Button>
+          </div>
+        </form>
+      </Form>
     </div>
 	)
 }
--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -2,7 +2,16 @@ import AuthForm from "./component/AuthForm"
 
 const Home = () => { 
 	return (
-		<div>
+		<div
+    className="
+    flex 
+    min-h-screen 
+    items-center
+    justify-center 
+    sm:px-6 
+    lg:px-8"
+  >
+    <div>
       <h1 
         className="
         text-center 
@@ -12,6 +21,7 @@ const Home = () => {
         text-gray-900"
       >ログイン</h1>
       <AuthForm/>
+    </div>
     </div>
 	)
 }
