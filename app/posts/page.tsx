a6f924d5ad990587c29247218f79ec9e5fdeb7c5
kei-1313 <candydog666@gmail.com>
2024-03-03 11:33:55 +0900

    ログアウトの作成

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,7 +1,15 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { signOut } from "next-auth/react"
+
 const Posts = () => { 
 	return (
 		<div>
-      Posts
+      <div className="flex justify-end px-10 mt-5">
+        <Button onClick={() => signOut({ callbackUrl: '/' })} size="lg">ログアウト</Button>
+      </div>
+      <h1>posts</h1>
     </div>
 	)
 }
