a31bf6730f769bca03a82d515c1cd272f7722a8f
kei-1313 <candydog666@gmail.com>
2024-03-10 12:46:33 +0900

    データベースから1投稿のコメントをすべて取得するためのAPIを作成

--- app/api/comments/route.ts
+++ app/api/comments/route.ts
@@ -2,7 +2,26 @@ import prisma from "@/app/libs/prismadb"
 import { NextResponse } from "next/server"
 
 export async function GET(request:Request) {
-  
+
+  try {
+    const { searchParams } = new URL(request.url)
+    const postId = searchParams.get("postId") ?? ""
+
+    const comment = await prisma.comment.findMany({
+      where: {
+        postId: postId,
+      }
+    })
+
+    if(!comment) {
+      return ""
+    }
+
+    return NextResponse.json(comment)
+    
+  } catch (error) {
+    console.error("Faild fetching comment", error);
+  }
 }
 
 export async function POST(request:Request) {
